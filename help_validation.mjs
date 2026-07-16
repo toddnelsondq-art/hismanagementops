9a4b2ff1c83aac2c3fcd93d2c5e39fb204a3cf13
toddnelsondq-art <toddnelson.dq@gmail.com>
2026-07-16 01:36:06 -0500

    Delete help_validation.mjs

--- help_validation.mjs
+++
@@ -1,5 +0,0 @@
-import { FileBlob, SpreadsheetFile } from "@oai/artifact-tool";
-const input = await FileBlob.load("C:/Users/tanel/OneDrive/Escritorio/DQ_Maintenance_Tracking_System_with_Location_Dropdowns.xlsx");
-const workbook = await SpreadsheetFile.importXlsx(input);
-console.log(workbook.help("dataValidation", { include: "index,examples,notes", maxChars: 8000 }).ndjson);
-console.log(workbook.help("dataValidations.add", { include: "index,examples,notes", maxChars: 8000 }).ndjson);
